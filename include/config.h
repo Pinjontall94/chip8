fcc827421278e567fcfa906d2a2f4af5970af580
Sam Johnson <sambjohnson94@gmail.com>
2024-01-29 01:54:05 -0800

    create chip8 memory

--- include/config.h
+++ include/config.h
@@ -5,4 +5,6 @@
 #define CHIP8_MEMORY_SIZE 4096
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
+#define CHIP8_WINDOW_MULTIPLIER 10
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "../include/chip8.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
@@ -5,43 +6,48 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window =
-        SDL_CreateWindow("Chip8 Window", SDL_WINDOWPOS_UNDEFINED,
-                SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
 
-    printf("hey girl! :3\n");
+  // Test if memory setter and getter work
+  struct chip8 chip8;
+  chip8_memory_set(&chip8.memory, 0x50, 'Z');
+  printf("%c\n", chip8_memory_get(&chip8.memory, 0x50));
 
-    SDL_Renderer *renderer =
-        SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *window = SDL_CreateWindow(
+      EMULATOR_WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+      CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER, SDL_WINDOW_SHOWN);
 
-    // Quit Condition Flag
-    int quit = 0;
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
-    // Event Loop
-    while (quit == 0) {
-        SDL_Event e;
-        SDL_WaitEvent(&e);
+  // Quit Condition Flag
+  int quit = 0;
 
-        if (e.type == SDL_QUIT) {
-            quit = 1;
-        }
+  // Event Loop
+  while (quit == 0) {
+    SDL_Event e;
+    SDL_WaitEvent(&e);
 
-        // Paint Screen Black
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-        SDL_Rect r;
-        r.x = 0;
-        r.y = 0;
-        r.w = 40;
-        r.h = 40;
-        SDL_RenderDrawRect(renderer, &r);
-        SDL_RenderPresent(renderer);
+    if (e.type == SDL_QUIT) {
+      quit = 1;
     }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
 
-    return 0;
+    // Paint Screen Black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+    SDL_Rect r;
+    r.x = 0;
+    r.y = 0;
+    r.w = 40;
+    r.h = 40;
+    SDL_RenderDrawRect(renderer, &r);
+    SDL_RenderPresent(renderer);
+  }
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  return 0;
 }
